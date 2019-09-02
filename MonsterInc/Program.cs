71cd46987fd9562c00ade3816c5d0d58cae7def0
Adeboye Azeez <AAzeez@spartaglobal.com>
2019-09-02 10:26:06 +0100

    MonsterInc1.0

---
+++ MonsterInc/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterInc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class MonsterStudent
+    {
+        private int id;
+        private string eyes;
+        private string hair;
+        private int scary_level;
+        private string special_ability;
+        
+
+
+
+        public MonsterStudent(int id, string eyes, string hair, int scary_level)
+            {
+            
+        }
+
+        public string Special_ability {
+            get {
+                return special_ability;
+            }
+            set
+            {
+                special_ability = value;
+            }
+        }
+
+    
+    }
+
+    class MonsterCohort
+    {
+    }
+}
